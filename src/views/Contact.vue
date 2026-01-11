93513e40707725a5cbd28c4a9d9c7d0553c7eeee
shaman06062548-spec <shaman06062548@gmail.com>
2026-01-11 16:44:24 +0700

    Initial commit

---
+++ src/views/Contact.vue
@@ -0,0 +1,7 @@
+<template lang="">
+    <div>
+        <h1>Contact to kitsana srirupabut</h1>
+        <h3>tel 063-421-0629</h3>
+        <img alt="Vue logo" src="../assets/images.jpg">
+    </div>
+</template>
