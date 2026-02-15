357603b44246f4b0114dede2dcede78caacbd06f
unknown <shaman06062548@gmail.com>
2026-02-15 18:26:50 +0700

    update v1

--- php_api/database/db_shop.sql
+++ php_api/database/db_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 25, 2026 at 12:33 PM
+-- Generation Time: Feb 15, 2026 at 12:25 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -41,10 +41,11 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`customer_id`, `firstName`, `lastName`, `phone`, `username`, `password`) VALUES
-(00000001, 'สมชาย', 'ใจดี', '0812345678', 'somchai', 'pass1234'),
 (00000002, 'วิภา', 'สุขสันต์', '0898765432', 'wipa', 'wipa2025'),
 (00000003, 'John', 'Doe', '0991122334', 'johnd', 'securepwd'),
-(00000004, 'KI', 'SER', '2515', 'KAISER', '$2y$10$HZnfYcCEvCvJs');
+(00000004, 'เมษา', 'เด็กดี', '038743690', 'mesa', '$2y$10$tSSl6/EruDffk'),
+(00000005, 'มานะ', 'มีใจ', '038754920', 'mana', '$2y$10$tA0rn9nxLE.i6q5U5qvgMuij0eJZJkNEQMTRn3lJsDECEt1jSq58u'),
+(00000006, 'ชูใจ', 'เด็กดี', '038743690', 'shoojai', '$2y$10$9JiaB6GjUeHH1pWQlsYpyebE7FVT8zZgINfMV2Ud16XKRlPGCtW5m');
 
 -- --------------------------------------------------------
 
@@ -53,11 +54,12 @@ INSERT INTO `customers` (`customer_id`, `firstName`, `lastName`, `phone`, `usern
 --
 
 CREATE TABLE `employee` (
-  `emp_id` int(8) UNSIGNED ZEROFILL NOT NULL,
+  `emp_id` int(10) UNSIGNED ZEROFILL NOT NULL,
   `full_name` varchar(100) NOT NULL,
   `department` varchar(50) NOT NULL,
   `salary` decimal(10,2) NOT NULL,
   `active` tinyint(1) DEFAULT 1,
+  `image` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -65,13 +67,14 @@ CREATE TABLE `employee` (
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`emp_id`, `full_name`, `department`, `salary`, `active`, `created_at`) VALUES
-(00000001, 'สมชาย ใจดี', 'IT', '35000.00', 1, '2026-01-18 10:30:21'),
-(00000002, 'สมหญิง ดีงาม', 'บัญชี', '28000.00', 1, '2026-01-18 10:30:21'),
-(00000003, 'อนันต์ สุขใจ', 'การตลาด', '25000.00', 0, '2026-01-18 10:30:21'),
-(00000004, 'สุดา พรมดี', 'ทรัพยากรบุคคล', '32000.00', 1, '2026-01-18 10:30:21'),
-(00000005, 'ki', 'iy', '125.00', 1, '2026-01-25 10:26:22'),
-(00000006, 'dsa', 'sa', '125.00', 1, '2026-01-25 10:29:45');
+INSERT INTO `employee` (`emp_id`, `full_name`, `department`, `salary`, `active`, `image`, `created_at`) VALUES
+(0000000001, 'สมชาย คนดี', 'การตลาด', '25000.00', 1, '', '2026-01-18 10:27:14'),
+(0000000002, 'สมหญิง ดีงาม', 'บัญชี', '28000.00', 1, '', '2026-01-18 10:27:14'),
+(0000000003, 'อนันต์ สุขใจ', 'การตลาด', '25000.00', 0, '', '2026-01-18 10:27:14'),
+(0000000004, 'สุดา พรมดี', 'ทรัพยากรบุคคล', '32000.00', 1, '', '2026-01-18 10:27:14'),
+(0000000005, 'นวรัตน์  เกิดผล', 'คอมพิวเตอร์', '25000.00', 1, '1771152837_577280151-1.jpg', '2026-01-25 09:43:49'),
+(0000002155, 'sda', 'das', '125.00', 0, '1771154188_577280151-1.jpg', '2026-02-15 11:16:28'),
+(0000002156, 'dsa', '16525', '23256.00', 0, '1771154464_577280151-1.jpg', '2026-02-15 11:21:04');
 
 -- --------------------------------------------------------
 
@@ -80,7 +83,7 @@ INSERT INTO `employee` (`emp_id`, `full_name`, `department`, `salary`, `active`,
 --
 
 CREATE TABLE `products` (
-  `product_id` int(11) NOT NULL,
+  `product_id` int(11) UNSIGNED ZEROFILL NOT NULL,
   `product_name` varchar(100) NOT NULL,
   `description` text DEFAULT NULL,
   `price` decimal(10,2) NOT NULL,
@@ -94,9 +97,15 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `description`, `price`, `image`, `stock`, `created_at`) VALUES
-(1, 'เสื้อยืดคอกลม', 'เสื้อยืดผ้าฝ้าย 100% สวมใส่สบาย', '199.00', '1.jpg', 50, '2026-01-25 10:45:40'),
-(2, 'กางเกงยีนส์', 'กางเกงยีนส์ทรงกระบอก สีฟ้าอ่อน', '799.00', '2.jpg', 30, '2026-01-25 10:45:40'),
-(3, 'รองเท้าผ้าใบ', 'รองเท้าผ้าใบสีขาว ใส่ได้ทุกโอกาส', '1299.00', '3.jpg', 20, '2026-01-25 10:45:40');
+(00000000001, 'เสื้อยืดคอกลม', 'เสื้อยืดผ้าฝ้าย 100% สวมใส่สบาย', '199.00', 'p1.jpg', 50, '2026-01-25 10:42:55'),
+(00000000002, 'กางเกงยีนส์', 'กางเกงยีนส์ทรงกระบอก สีฟ้าอ่อน', '799.00', 'p2.jpg', 30, '2026-01-25 10:42:55'),
+(00000000003, 'รองเท้าผ้าใบ', 'รองเท้าผ้าใบสีขาว ใส่ได้ทุกโอกาส', '1299.00', 'p3.jpg', 20, '2026-01-25 10:42:55'),
+(00000000004, 'พ่อหนุ่ม', 'ชายแท้', '999999.00', '1771145327_577280151-1.jpg', 1, '2026-02-15 08:48:47'),
+(00000000005, 'หฟ', 'ฟก', '123.00', '1771147982_577280151-1.jpg', 2, '2026-02-15 09:33:02'),
+(00000000006, '01', '00', '989.00', '1771148031_577280151-1.jpg', 1, '2026-02-15 09:33:51'),
+(00000000007, '14', '25', '22.00', '1771148129_577280151-1.jpg', 1, '2026-02-15 09:35:29'),
+(00000000008, '25', '15', '1.00', '1771149786_577280151-1.jpg', 1, '2026-02-15 10:03:06'),
+(00000000009, 'dsa', 'ds', '12.00', '1771149819_577280151-1.jpg', 12, '2026-02-15 10:03:39');
 
 -- --------------------------------------------------------
 
@@ -105,8 +114,8 @@ INSERT INTO `products` (`product_id`, `product_name`, `description`, `price`, `i
 --
 
 CREATE TABLE `tb_type` (
-  `type_id` int(11) NOT NULL,
-  `type_name` varchar(30) NOT NULL
+  `type_id` int(11) NOT NULL COMMENT 'รหัสสินค้า',
+  `type_name` varchar(30) NOT NULL COMMENT 'ชื่อประเภท'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -153,25 +162,25 @@ ALTER TABLE `tb_type`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `customer_id` int(8) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `customer_id` int(8) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `emp_id` int(8) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `emp_id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2157;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `product_id` int(11) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tb_type`
 --
 ALTER TABLE `tb_type`
-  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสสินค้า', AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
